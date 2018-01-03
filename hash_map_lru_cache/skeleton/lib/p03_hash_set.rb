4654011bede23144586f83b02135293fe0cb3784
aahmad94 <aka5vm@virginia.edu>
2018-01-02 17:25:09 -0800

    add HashSet

--- hash_map_lru_cache/skeleton/lib/p03_hash_set.rb
+++ hash_map_lru_cache/skeleton/lib/p03_hash_set.rb
@@ -9,18 +9,28 @@ class HashSet
   end
 
   def insert(key)
+    resize! if @count == num_buckets
+    return false if self[key].include?(key)
+    self[key].push(key)
+    @count += 1
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def remove(key)
+    if self[key].include?(key)
+      self[key].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
-  def [](num)
+  def [](val)
     # optional but useful; return the bucket corresponding to `num`
+    @store[val.hash % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +38,13 @@ class HashSet
   end
 
   def resize!
+    new_num_buckets = num_buckets * 2
+    new_store = Array.new(new_num_buckets) { Array.new }
+    @store.each do |bucket|
+      bucket.each do |int|
+        new_store[int % new_num_buckets].push(int)
+      end
+    end
+    @store = new_store
   end
 end
