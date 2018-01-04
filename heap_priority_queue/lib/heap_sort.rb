96a62ddf5e18a93a8af8c3dfba36bfb36338dad2
aahmad94 <aka5vm@virginia.edu>
2018-01-04 01:38:30 -0800

    add heap_sort and k_largest_elements

--- heap_priority_queue/lib/heap_sort.rb
+++ heap_priority_queue/lib/heap_sort.rb
@@ -1,6 +1,16 @@
 require_relative "heap"
-
 class Array
   def heap_sort!
+    heap = BinaryMinHeap.new
+
+    while self.length > 0
+      heap.push(self.pop)
+    end
+
+    while heap.count > 0
+      self << heap.extract
+    end
+
+    self
   end
 end
--- heap_priority_queue/lib/k_largest_elements.rb
+++ heap_priority_queue/lib/k_largest_elements.rb
@@ -1,5 +1,6 @@
 require_relative 'heap'
+require_relative "heap_sort"
 
 def k_largest_elements(array, k)
-
+    array.heap_sort![(array.length - k)..-1]
 end
