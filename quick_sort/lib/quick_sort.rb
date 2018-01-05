d64361cd02703008cd4f79974fdce27da51c4008
aahmad94 <aka5vm@virginia.edu>
2018-01-04 22:24:01 -0800

    add partition for quicksort

--- quick_sort/lib/quick_sort.rb
+++ quick_sort/lib/quick_sort.rb
@@ -3,7 +3,15 @@ class QuickSort
   # case O(n**2).
 
   # Not in-place. Uses O(n) memory.
-  def self.sort1(array)
+  def self.sort1(array, &prc)
+    prc ||= proc { |a, b| a <=> b }
+    return array if array.length < 2
+
+    pivot = array.first
+    left = array[1..-1].select { |el| prc.call(el, pivot) == -1 }
+    right = array[1..-1].select { |el| prc.call(el, pivot) != -1 }
+    
+    self.sort1(left, &prc) + [pivot] + self.sort1(right, &prc)
   end
 
   # In-place.
@@ -11,5 +19,22 @@ class QuickSort
   end
 
   def self.partition(array, start, length, &prc)
+    prc ||= proc { |a, b| a <=> b }
+
+    pivot = array[start]
+    pivot_idx = start
+
+    # start + 1 because we onlly deal with the pivot val's position at the end of the partition
+    (start + 1..start + length - 1).each do |idx|
+      # if array[idx]val < pivot val, swap items and move barrier (pivot_idx) up one 
+      if prc.call(pivot, array[idx]) == 1 
+        array[pivot_idx + 1], array[idx] = array[idx], array[pivot_idx + 1]
+        pivot_idx += 1
+      end 
+    end 
+
+    # swap pivot directly to the left of where the barrier now is (pivot_idx) to put pivot at it's correct index 
+    array[pivot_idx], array[start] = array[start], array[pivot_idx]
+    pivot_idx
   end
 end
