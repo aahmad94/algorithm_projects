4ba236e16c04589f08fb614f4b583b441239306c
aahmad94 <aka5vm@virginia.edu>
2018-01-09 11:23:41 -0800

    add annotations to topological sort

--- graphs_and_topologoical_sort/lib/topological_sort.rb
+++ graphs_and_topologoical_sort/lib/topological_sort.rb
@@ -5,19 +5,24 @@ require_relative 'graph'
 def topological_sort(vertices)
   sorted = []
   queue = []
-
+  
+  # enqueue any vectors that don't have any in_edges 
   vertices.each do |vertex|
     if vertex.in_edges.empty?
         queue.push(vertex)
     end 
   end
 
+  # process queued items
+  # append to sorted array the first-in vertice and dequeue 
   until queue.empty?
-    current = queue.shift
-    sorted.push(current)
+    vertice = queue.shift
+    sorted.push(vertice)
 
-    until current.out_edges.empty?
-      edge = current.out_edges.first
+    # find all out_edges of dequeued vertice and destroy them
+    # enqueue vertices that now have no remaining in_edges
+    until vertice.out_edges.empty?
+      edge = vertice.out_edges.[0]
       to_vertex = edge.to_vertex
       edge.destroy!
       if to_vertex.in_edges.empty?
