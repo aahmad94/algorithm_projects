98624b8ca27bb4727668c02aa35fb0ef0ae45452
aahmad94 <aka5vm@virginia.edu>
2018-02-24 01:48:28 -0800

    add early term in valid_par prob if odd s

--- notes/amazon/valid_parenthesis.js
+++ notes/amazon/valid_parenthesis.js
@@ -1,19 +1,22 @@
 var isValid = function (s) {
 
+  if (s.length % 2 === 1) {
+    return false;
+  }
+
   const stack = new Array();
   const open = new Set(["(", "{", "["]);
   const close = new Set([")", "}", "]"]);
-  const map = { ")": "(", "}": "{", "]": "[" }
+  const map = { ")": "(", "}": "{", "]": "[" };
 
   for (let i = 0; i < s.length; i++) {
     let chr = s[i];
     if (close.has(chr)) {
-      const popped = stack.pop();
-      if (popped !== map[chr]) {
+      if (stack.pop() !== map[chr]) {
         return false;
       }
     } else if (open.has(chr)) {
-      stack.push(chr);
+        stack.push(chr);
       } else {
         return false;
     }
