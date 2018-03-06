060fa9a9611ac5ca869b2c5a62989399f2ee9e26
aahmad94 <aka5vm@virginia.edu>
2018-03-05 19:03:04 -0800

    add number_compliment soln

---
+++ notes/microsoft/number_compliment.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var findComplement = function (num) {
+  const binaryNum = toBinary(num);
+  const binaryCompliment = generateCompliment(binaryNum);
+  const baseTenCompliment = binaryToBaseTen(binaryCompliment);
+  return baseTenCompliment;
+};
+
+const toBinary = function (num) {
+  const remainder = num % 2;
+  const quotient = Math.floor(num / 2);
+  if (quotient === 0) {
+    return '1';
+  }
+  return toBinary(quotient).concat(remainder.toString());
+};
+
+const generateCompliment = function (str) {
+  const nums = str.split("");
+  const compliment = nums.map((num) => {
+    if (num === "0") {
+      return 1;
+    } else {
+      return 0;
+    }
+  })
+  return compliment.join("");
+};
+
+const binaryToBaseTen = function (strNum) {
+  let result = 0;
+  let j = 1;
+  for (let i = strNum.length - 1; i >= 0; i-- , j *= 2) {
+    const digit = Number.parseInt(strNum[i]);
+    if (digit === 1) {
+      result += j;
+    }
+  }
+  return result;
+};
