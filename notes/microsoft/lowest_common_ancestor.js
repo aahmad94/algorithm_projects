9c92747a9f4982be0e7ce74383a4e34973dd87a0
aahmad94 <aka5vm@virginia.edu>
2018-03-19 00:15:37 -0700

    add notes to lowest common ancestor problem

--- notes/microsoft/lowest_common_ancestor.js
+++ notes/microsoft/lowest_common_ancestor.js
@@ -13,3 +13,14 @@ var lowestCommonAncestor = function (root, p, q) {
     return right;
   }
 };
+
+// const lowestCommonAncestor = (root, p, q) => {
+//   if (root === null || root === p || root === q) {
+//     return root;
+//   }
+//   const left = lowestCommonAncestor(root.left, p, q);
+//   const right = lowestCommonAncestor(root.right, p, q);
+//   if (left && !right) { return left; }
+//   if (right && !left) { return right; }
+//   if (left && right) { return root; }
+// };
