6ea7b427c8acc07b24e9a312bf962f926d702b2c
aahmad94 <aka5vm@virginia.edu>
2018-05-14 14:50:25 -0700

    add three_sum problem

---
+++ notes/three_sum.js
@@ -0,0 +1,20 @@
+var threeSum = function (nums) {
+  const result = [];
+  const set = new Set(nums);
+  const compliments = new Set();
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+
+      const sum = nums[i] + nums[j];
+      const compliment = -1 * sum;
+
+      if (set.has(compliment) && !compliments.has(compliment)) {
+        result.push([nums[i], nums[j], compliment]);
+      }
+      compliments.add(compliment);
+
+    }
+  }
+  return result;
+};
