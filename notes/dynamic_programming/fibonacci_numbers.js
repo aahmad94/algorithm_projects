22bbb61b8edf08b418e742a97219988687fd1d79
aahmad94 <aka5vm@virginia.edu>
2018-05-31 00:39:30 -0700

    annotate fiboonacci_numbers dp soln

--- notes/dynamic_programming/fibonacci_numbers.js
+++ notes/dynamic_programming/fibonacci_numbers.js
@@ -1,9 +1,9 @@
-// generate fibonnaci numbers via dynamic programming
-// dp - break problem into subproblems and then
+// generate nth fibonnaci number via dynamic programming
+// dynamic programming - break problem into subproblems and then
 // recursively find solns to the subproblems
 const fib = (n) => {
   // initialize fib map w/ seed values
-  // use to store soln to subproblems
+  // use to store soln to subproblems (memoization)
   let map = [0, 1, 1];
 
   // closure will populate the map recursively
@@ -12,9 +12,21 @@ const fib = (n) => {
       // subproblem is f(n-1) + f(n-2)
       map[n] = calc(n - 1) + calc(n - 2);
     }
+    // instead of recalculating same problems
+    //just lookup previous soln in map
     return map[n];
   };
   return calc(n);
 };
 
-console.log(fib(4));
+// fib seq: 1, 1, 2, 3, 5, 8, 13, 21...
+// ex. problem tree w/o dynamic programming time complexity: O(2^n)
+//    5
+//   3  2
+//  2 1 1 1
+// 1 1
+
+// no longer need to recalculate the two node
+// time complexity: O(n)
+// at most n recursive calls are made
+console.log(fib(5)); // => 5
