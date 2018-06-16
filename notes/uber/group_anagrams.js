7e7963e09e8b847cb83aab220b6806b3763cabc4
aahmad94 <aka5vm@virginia.edu>
2018-06-16 14:26:43 -0700

    annotate group_anagrams problem

--- notes/uber/group_anagrams.js
+++ notes/uber/group_anagrams.js
@@ -1,11 +1,15 @@
 var groupAnagrams = function (words) {
   const anagramsMap = {};
   words.forEach(word => {
+    // sort each word to see if it exists in our map
     const sortedWord = word.split("").sort().join();
+    // if the sorted word exists in our map,
+    // push the original word into the arr
     if (anagramsMap[sortedWord]) {
       anagramsMap[sortedWord].push(word);
     } else {
-      anagramsMap[sortedWord] = [word];
+      // else set the initial key to be the sorted word
+      anagramsMap[sortedWord] = [sortedWord];
     }
   });
   return Object.values(anagramsMap);
