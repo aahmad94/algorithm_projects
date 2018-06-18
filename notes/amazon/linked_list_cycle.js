ecf35d33535caf7c0737cdeeb14b8d5e9348c3f0
aahmad94 <aka5vm@virginia.edu>
2018-06-17 18:00:36 -0700

    add linked list cycle problem

---
+++ notes/amazon/linked_list_cycle.js
@@ -0,0 +1,15 @@
+var hasCycle = function (head) {
+  const set = new Set();
+
+  while (head) {
+    if (set.has(head)) {
+      return true;
+    } else {
+      set.add(head);
+    }
+
+    head = head.next;
+  }
+
+  return false;
+};
