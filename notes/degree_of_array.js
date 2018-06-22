5792e87c4b3da75440acfffbe9221af50385f73b
aahmad94 <aka5vm@virginia.edu>
2018-06-22 13:02:53 -0700

    uncomment initial soln for degree of arr

--- notes/degree_of_array.js
+++ notes/degree_of_array.js
@@ -1,21 +1,21 @@
-const findShortestSubArray = (arr) => {
+const findShortestSubArray1 = (arr) => {
   const firstIndicies = {};
   const lastIndicies = {};
   const counts = {};
   let max = 0;
   for (let i = 0; i < arr.length; i++) {
-    const j = arr[i];
-    counts[j] = (counts[j] || 0) + 1;
-    max = Math.max(max, counts[j]);
-    if (firstIndicies[j] === undefined) {
-      firstIndicies[j] = i;
+    const chr = arr[i];
+    counts[chr] = (counts[chr] || 0) + 1;
+    max = Math.max(max, counts[chr]);
+    if (firstIndicies[chr] === undefined) {
+      firstIndicies[chr] = i;
     }
-    lastIndicies[j] = i;
+    lastIndicies[chr] = i;
   }
   let ans = arr.length;
-  for (const j in counts) {
-    if (counts[j] === max) {
-      ans = Math.min(ans, lastIndicies[j] - firstIndicies[j] + 1);
+  for (const chr in counts) {
+    if (counts[chr] === max) {
+      ans = Math.min(ans, lastIndicies[chr] - firstIndicies[chr] + 1);
     }
   }
   return ans;
@@ -23,50 +23,50 @@ const findShortestSubArray = (arr) => {
 
 
 // initial soln (time complexity: O(n), space complexity O(n))
-// var findShortestSubArray = function (nums) {
-//   const map = {};
-//   let degree;
-//   const degreeNums = new Set();
-//   let shortestLength = nums.length;
+var findShortestSubArray2 = function (nums) {
+  const map = {};
+  let degree;
+  const degreeNums = new Set();
+  let shortestLength = nums.length;
 
-//   for (let i = 0; i < nums.length; i++) {
-//     if (map[nums[i]]) {
-//       map[nums[i]]++;
-//     } else {
-//       map[nums[i]] = 1;
-//     }
-//   }
+  for (let i = 0; i < nums.length; i++) {
+    if (map[nums[i]]) {
+      map[nums[i]]++;
+    } else {
+      map[nums[i]] = 1;
+    }
+  }
 
-//   degree = Math.max(...(Object.values(map)));
+  degree = Math.max(...(Object.values(map)));
 
-//   for (let i = 0; i < nums.length; i++) {
-//     if (map[nums[i]] === degree) {
-//       degreeNums.add(nums[i]);
-//     }
-//   }
+  for (let i = 0; i < nums.length; i++) {
+    if (map[nums[i]] === degree) {
+      degreeNums.add(nums[i]);
+    }
+  }
 
-//   for (let degreeNum of degreeNums) {
-//     shortestLength = Math.min(shortestLength, findShortestSubArrayOfNum(nums, degree, degreeNum));
-//   }
+  for (let degreeNum of degreeNums) {
+    shortestLength = Math.min(shortestLength, findShortestSubArrayOfNum(nums, degree, degreeNum));
+  }
 
-//   return shortestLength;
-// };
+  return shortestLength;
+};
 
-// const findShortestSubArrayOfNum = (nums, degree, degreeNum) => {
-//   let shortestLength = nums.length;
-//   let currCt = 0;
-//   for (let i = 0, j = 0; j < nums.length;) {
-//     if (nums[j] === degreeNum) {
-//       currCt++;
-//       if (currCt === degree) {
-//         shortestLength = Math.min(shortestLength, j - i + 1);
-//         while (nums[i] !== degreeNum) {
-//           i++;
-//           shortestLength = Math.min(shortestLength, j - i + 1);
-//         }
-//       }
-//     }
-//     j++;
-//   }
-//   return shortestLength;
-// };
+const findShortestSubArrayOfNum = (nums, degree, degreeNum) => {
+  let shortestLength = nums.length;
+  let currCt = 0;
+  for (let i = 0, chr = 0; chr < nums.length;) {
+    if (nums[chr] === degreeNum) {
+      currCt++;
+      if (currCt === degree) {
+        shortestLength = Math.min(shortestLength, chr - i + 1);
+        while (nums[i] !== degreeNum) {
+          i++;
+          shortestLength = Math.min(shortestLength, chr - i + 1);
+        }
+      }
+    }
+    chr++;
+  }
+  return shortestLength;
+};
