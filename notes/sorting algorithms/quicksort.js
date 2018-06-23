c4c786aacf4469186f8bcf50f937b355208e8246
aahmad94 <aka5vm@virginia.edu>
2018-06-22 22:53:20 -0700

    add random pivot for quicksort

--- notes/sorting algorithms/quicksort.js
+++ notes/sorting algorithms/quicksort.js
@@ -1,6 +1,19 @@
 const quickSort = (arr) => {
   if (arr.length <= 1) return arr;
-  const left = quickSort(arr.slice(1).filter(num => num <= arr[0]));
-  const right = quickSort(arr.slice(1).filter(num => num > arr[0]));
-  return left.concat([arr[0]], right);
+
+  const pivotIdx = Math.round((arr.length - 1) * Math.random());
+  const pivot = arr[pivotIdx];
+  const arrNoPivot = arr.slice(0, pivotIdx).concat(arr.slice(pivotIdx + 1));
+
+  const left = quickSort(arrNoPivot.filter(num => num <= pivot));
+  const right = quickSort(arrNoPivot.filter(num => num > pivot));
+  return left.concat([pivot], right);
 };
+
+const arr = [20, 100, 72, 92, 22, 21, 2];
+console.log(quickSort(arr));
+
+// space: O(n * log(n) / 2)
+// time: O(n * log(n)) -- average case
+// time O(n * (n - 1) / 2) = O(n^2)  -- worst case, when pivot doesn't change
+// and input is reverse sorted 
