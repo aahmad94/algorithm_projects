9ee21d625570ec302f6009a4da355615b4e66450
aahmad94 <aka5vm@virginia.edu>
2018-06-24 23:49:12 -0700

    restructure merge two sorted arrays to make logic clear

--- notes/microsoft/merge_two_sorted_arrs.js
+++ notes/microsoft/merge_two_sorted_arrs.js
@@ -4,9 +4,12 @@ var merge = function (nums1, m, nums2, n) {
   let k = m + n - 1;
   while (j >= 0) {
     if (nums1[i] > nums2[j]) {
-      nums1[k--] = nums1[i--];
+      nums1[k] = nums1[i];
+      i--;
     } else {
-      nums1[k--] = nums2[j--];
+      nums1[k] = nums2[j];
+      j--;
     }
+    k--;
   }
 };
