577bb1e1404cde379f4a524003c8f35aa7d39c11
aahmad94 <aka5vm@virginia.edu>
2018-06-25 19:04:13 -0700

    add iterative binary search solution

--- notes/binary_search.js
+++ notes/binary_search.js
@@ -15,3 +15,23 @@ const binarySearch = function (nums, target) {
 };
 
 module.exports = binarySearch;
+
+let binarySearchIterative = function (a, key) {
+
+  let low = 0;
+  let high = a.length - 1;
+  while (low <= high) {
+    let mid = low + Math.floor((high - low) / 2);
+    if (a[mid] === key) {
+      return mid;
+    }
+
+    if (key < a[mid]) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return -1;
+};
