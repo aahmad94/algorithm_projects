f6521c27f83a3604d9bd36b0b81c70c6e1fa8066
aahmad94 <aka5vm@virginia.edu>
2018-07-09 22:43:26 -0700

    update election winner problem to sort candidates by name

--- notes/amazon/electionWinner.js
+++ notes/amazon/electionWinner.js
@@ -1,17 +1,19 @@
 function electionWinner(votes) {
   const voteCts = {};
   const sortedVoteCts = [];
+  let max = 0;
   for (let i = 0; i < votes.length; i++) {
     voteCts[votes[i]] = (voteCts[votes[i]] || 0) + 1;
+    if (voteCts[votes[i]] > max) max = voteCts[votes[i]]; 
   }
-  for (const candidate in voteCts) {
-    sortedVoteCts.push([candidate, voteCts[candidate]]);
-  }
-  sortedVoteCts.sort(function (a, b) {
-    return a[0] - b[0];
-  });
-  console.log({
-    sortedVoteCts
-  });
 
+  for (const candidate in voteCts) {
+    if (voteCts === max) {
+      sortedVoteCts.push([candidate, voteCts[candidate]]);
+    }
+  }
+
+  sortedVoteCts.sort();
+
+  return sortedVoteCts[sortedVoteCts.length - 1] ? sortedVoteCts[sortedVoteCts.length - 1] : -1;
 }
