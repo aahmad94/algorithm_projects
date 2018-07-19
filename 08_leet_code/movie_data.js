ec7f2c801486df5a972de236ed6d554f1fee2e24
aahmad94 <aka5vm@virginia.edu>
2018-07-19 16:46:41 -0700

    add fb num ways to decode str dp solncd

--- 08_leet_code/movie_data.js
+++ 08_leet_code/movie_data.js
@@ -1,26 +1,44 @@
 const https = require('https');
 
-const getContent = async (cb) => {
-  const searchTerm = "spiderman";
-  const urlStr = `https://jsonmock.hackerrank.com/api/movies/search/?Title=${searchTerm}`;
-  await https.get(urlStr, (response) => {
-  // console.log('statusCode:', response.statusCode); // statusCode: 200
-    response.on('data', (d) => {
-       cb(Array.from(JSON.parse(d).data));
-    });
-    response.on('end', () => (console.log("END")));
-  });
-  return store;
-};
-
-const cb = (toReturn) => {
-  return toReturn;
-};
-
-getContent(cb);
-
-// const movieData = () => {
-//   return getContent().then((data) => data);
+// const getContent = async (cb) => {
+//   const searchTerm = "spiderman";
+//   const urlStr = `https://jsonmock.hackerrank.com/api/movies/search/?Title=${searchTerm}`;
+//   await https.get(urlStr, (response) => {
+//   // console.log('statusCode:', response.statusCode); // statusCode: 200
+//     response.on('data', (d) => {
+//        cb(Array.from(JSON.parse(d).data));
+//     });
+//     response.on('end', () => (console.log("END")));
+//   });
+//   return store;
 // };
 
-// movieData();
+// const cb = (toReturn) => {
+//   return toReturn;
+// };
+
+// getContent(cb);
+
+// // const movieData = () => {
+// //   return getContent().then((data) => data);
+// // };
+
+// // movieData();
+
+function getTopicCount(topic) {
+  const params = {
+    count: 0
+  };
+  const urlStr = `https://en.wikipedia.org/wiki/${topic}`
+  https.get(urlStr, resp => {
+    resp.on('data', data => {
+      updateCtCB(data, params);
+    });
+  });
+}
+
+const updateCtCB = (data, params) => {
+  console.log(data);
+};
+
+console.log(getTopicCount("pizza"));
