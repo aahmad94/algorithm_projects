8166af3b1c09e7f916797909452caab437dbfca7
aahmad94 <aka5vm@virginia.edu>
2018-07-20 10:52:09 -0700

    update max profit / stocks problem

--- 08_leet_code/max_profit.js
+++ 08_leet_code/max_profit.js
@@ -1,27 +1,22 @@
 var maxProfit = function (prices) {
   let count = 0;
-  for (let i = 0, j = 1; i < prices.length - 1;) {
+  for (let i = 0, j = 1; j < prices.length;) {
     if (prices[i + 1] && prices[i + 1] < prices[i]) {
       i++;
       j = i + 1;
       continue;
     }
-
-    if (prices[j + 1] && prices[j + 1] > prices[j]) {
+    if (prices[j] < prices[i]) {
+      i = j;
       j++;
       continue;
     }
-
-    if (prices[j] > prices[i]) {
-      count += prices[j] - prices[i];
-      i = j + 1;
-      j = i + 1;
+    if (prices[j] - prices[i] > count) {
+      count = prices[j] - prices[i];
+      j++;
       continue;
     }
-
-    i++;
-    j = i + 1;
-
+    j++;
   }
   return count;
 };
