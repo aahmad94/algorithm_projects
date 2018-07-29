ade8fafdf913e778574e623c3ae1f84aab269ffc
aahmad94 <aka5vm@virginia.edu>
2018-07-29 01:02:11 -0700

    fix max non adj sum for neg nums

--- 08_leet_code/dynamic_programming/max_non_adj_sum.js
+++ 08_leet_code/dynamic_programming/max_non_adj_sum.js
@@ -3,17 +3,23 @@ const maxNonAdjSum = (nums) => {
   let max = 0;
 
   for (let i = 0; i < dp.length; i++) {
+    let first = 0;
+    let second = 0;
     if (dp[i - 2]) {
-      dp[i] += dp[i - 2];
+      first = Math.max(dp[i], dp[i] + dp[i - 2], dp[i-2]);
     }
     if (dp[i - 3]) {
-      dp[i] += dp[i - 3];
+      second = Math.max(dp[i], dp[i] + dp[i - 3], dp[i-3]);
     }
-    
+    if (first > 0 || second > 0) {
+      dp[i] = Math.max(first, second);
+    }
     max = Math.max(max, dp[i]);
   }
 
   return max;
 };
 
+console.log(maxNonAdjSum([10, 11, -6, 2, 5, 1]));
 console.log(maxNonAdjSum([5, 1, 1, 5]));
+
