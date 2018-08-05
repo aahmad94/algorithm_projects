69f4b41444cc6ec3fa3caf82641d925ed590bf45
aahmad94 <aka5vm@virginia.edu>
2018-08-04 18:32:06 -0700

    fix frogjump naieve soln

--- 08_leet_code/google/frog_jump/Solution.java
+++ 08_leet_code/google/frog_jump/Solution.java
@@ -1,30 +1,26 @@
 public class Solution {
   public static void main(String[] args) {
-    int[] river = {0,1,3,5,6,8,12,17}; // true
-    // int[] river = {0,1,2,3,4,8,9,11};
-    System.out.println(frogJump(river));
+    int[] stones = {0,1,3,5,6,8,12,17}; // true
+    // int[] stones = {0,1,2,3,4,8,9,11}; // true
+    System.out.println(frogJump(stones));
   }
 
-  public static Boolean frogJump(int[] river) {
-    return frogJumpHelper(river, 0, 0);
+  public static Boolean frogJump(int[] stones) {
+    return frogJumpHelper(stones, 0, 0);
   }
 
-  private static Boolean frogJumpHelper(int[] river, int idx, int jumpDistance) {
-    System.out.println(river[idx]);
-    System.out.println(jumpDistance);
-    System.out.println("----------");
-    if (idx == river.length - 1) {
-      if (jumpDistance == river[idx]) {
-        return true;
-      } else {
-        return false;
+  private static Boolean frogJumpHelper(int[] stones, int idx, int jumpSize) {
+    for (int i = idx + 1; i < stones.length; i++) {
+      int gap = stones[i] - stones[idx];
+      if (gap >= jumpSize - 1 && gap <= jumpSize + 1) {
+        if (frogJumpHelper(stones, i, gap)) {
+          return true;
+        }
       }
     }
-    Boolean toReturn = (
-      frogJumpHelper(river, idx + 1, river[idx] + jumpDistance + 1) ||
-      frogJumpHelper(river, idx + 1, river[idx] + jumpDistance - 1)  
-    );
-    
-    return toReturn;
-   }
+    return idx == stones.length - 1;  
+  }
 }
+
+// time complelxity O(3^n)
+// space complexity: O(n)
