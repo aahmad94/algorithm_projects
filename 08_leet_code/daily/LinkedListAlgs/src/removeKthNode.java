4995efb26b829860526bd3f9b1ae0e9369e567bf
aahmad94 <aka5vm@virginia.edu>
2018-08-16 17:14:02 -0700

    fix remove kth node from linked list

--- 08_leet_code/daily/LinkedListAlgs/src/removeKthNode.java
+++ 08_leet_code/daily/LinkedListAlgs/src/removeKthNode.java
@@ -10,21 +10,33 @@ public class removeKthNode {
 		nums.add(4);
 		
 		ListNode list = ListNode.arrToList(nums);
-		System.out.println(remove(list, 0).getVal());
+		ListNode newList = remove(list, 0);
+		StringBuilder sb = new StringBuilder();
+		while (newList != null) {
+			sb.append(newList.getVal());
+			sb.append("\t");
+			newList = newList.getNext();
+		}
+		System.out.println(sb);
 	}
 	
 	public static ListNode remove(ListNode list, int k) {
-		ListNode head = list;
-		ListNode rear = list;
-		ListNode front = list.getNext();
+		if (list == null) return null;
+		ListNode head = new ListNode(0);
+		head.setNext(list);
+		ListNode rear = head;
+		ListNode front = list;
 		int i = 0;
-		while(front != null && i < k) {
-			rear = front.getNext();
+		while(i < k) {
+			rear = rear.getNext();
 			front = front.getNext();
+			i++;
 		}
-		if (front != null && front.getNext() != null) {
+		if (front != null) {
 			rear.setNext(front.getNext());
+		} else {
+			rear.setNext(null);
 		}
-		return head;
+		return head.getNext();
 	}
 }
